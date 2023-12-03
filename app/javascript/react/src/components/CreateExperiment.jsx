ab5579bf1994b50af8d7a4d73ef79f29bc2b9671
Fernando de Alcantara <45339608+fernandodealcantara@users.noreply.github.com>
2023-12-03 14:09:54 -0300

    Atualiza componente de criar o experimento
    Remove criação manual das trials

--- app/javascript/react/src/components/CreateExperiment.jsx
+++ app/javascript/react/src/components/CreateExperiment.jsx
@@ -1,65 +1,47 @@
 import React, { useEffect, useState } from "react";
+import toast from "react-simple-toasts";
+import 'react-simple-toasts/dist/theme/success.css';
+import 'react-simple-toasts/dist/theme/failure.css';
+
 
 const CreateExperiment = () => {
-	const [nameExperiment, setNameExperiment] = useState(localStorage.getItem("nameExperiment") || "");
+	const [experimentName, setExperimentName] = useState(
+		localStorage.getItem("experimentName") || ""
+	);
 	const [factors, setFactors] = useState(JSON.parse(localStorage.getItem("factors")) || {});
-	const [trials, setTrials] = useState(JSON.parse(localStorage.getItem("trials")) || {});
+	const [value, setValue] = useState("");
 
-	addFactor = (name, value) => {
-		// if name and value are not empty and name is not already a factor
-		if (name.length > 0 && value.length > 0 && !Object.keys(factors).includes(name)) {
-			setFactors({ ...factors, [name]: value });
+	addFactor = (name) => {
+		if (name.length > 0 && !Object.keys(factors).includes(name)) {
+			setFactors({ ...factors, [name]: [] });
 		}
 	};
 
 	removeFactor = (name) => {
 		const newFactors = { ...factors };
-
-		// remove factor from trials
-		const newTrials = { ...trials };
-		Object.keys(trials).forEach((trial) => {
-			if (newTrials[trial].includes(name)) {
-				newTrials[trial].splice(newTrials[trial].indexOf(name), 1);
-			}
-		});
-
 		delete newFactors[name];
-
 		setFactors(newFactors);
-		setTrials(newTrials);
 	};
 
-	addTrial = (name) => {
-		if (name.length > 0 && !Object.keys(trials).includes(name)) {
-			setTrials({ ...trials, [name]: [] });
+	addValueToFactor = (factorName, value) => {
+		const newFactors = { ...factors };
+		if (newFactors[factorName] && !newFactors[factorName].includes(value) && value.length > 0) {
+			newFactors[factorName].push(value);
 		}
+		setFactors(newFactors);
 	};
 
-	removeTrial = (name) => {
-		const newTrials = { ...trials };
-		delete newTrials[name];
-		setTrials(newTrials);
-	};
-
-	addFactorToTrial = (trialName, factorName) => {
-		const newTrials = { ...trials };
-		// add if not already in trial and factor exists
-		if (!newTrials[trialName]?.includes(factorName) && Object.keys(factors).includes(factorName)) {
-			newTrials[trialName].push(factorName);
+	removeValueFromFactor = (factorName, value) => {
+		const newFactors = { ...factors };
+		if (newFactors[factorName]?.includes(value)) {
+			newFactors[factorName] = newFactors[factorName].filter((v) => v !== value);
 		}
-		setTrials(newTrials);
+		setFactors(newFactors);
 	};
 
-	removeFactorFromTrial = (trialName, factorName) => {
-		const newTrials = { ...trials };
-		if (newTrials[trialName]?.includes(factorName)) {
-			newTrials[trialName].splice(newTrials[trialName].indexOf(factorName), 1);
-		}
-		setTrials(newTrials);
-	};
 
 	submitExperiment = () => {
-		const data = { nameExperiment, factors, trials };
+		const data = { experimentName, factors };
 
 		// post /experiments
 		fetch("/experiment/create", {
@@ -71,28 +53,36 @@ const CreateExperiment = () => {
 			credentials: "same-origin",
 		})
 			.then(async (res) => {
-				const parse = await res.text();
+				const parse = JSON.parse(await res.text());
 				console.log(parse);
 				return parse;
 			})
 			.then((res) => {
-				console.log(res);
-				// clearExperiment();
-			});
+				if (res?.error) {
+					toast("Erro ao criar o experimento!", {
+						position: "top-right", theme: "failure"
+					})
+					return;
+				}
+				toast("Experimento criado com sucesso!", { position: "top-right", theme: "success" });
+				clearExperiment();
+			}).catch((err) => {
+				toast("Erro ao criar o experimento!", {
+					position: "top-right", theme: "failure"
+				})
+			})
 	};
 
 	clearExperiment = () => {
-		setNameExperiment("");
+		setExperimentName("");
 		setFactors({});
-		setTrials({});
 	};
 
 	// Cache values in local storage so they are not lost on page refresh
 	useEffect(() => {
-		localStorage.setItem("nameExperiment", nameExperiment);
+		localStorage.setItem("experimentName", experimentName);
 		localStorage.setItem("factors", JSON.stringify(factors));
-		localStorage.setItem("trials", JSON.stringify(trials));
-	}, [nameExperiment, factors, trials]);
+	}, [experimentName, factors]);
 
 	return (
 		<div className="outer-container">
@@ -101,57 +91,46 @@ const CreateExperiment = () => {
 				<form>
 					<div>
 						<input
+							id="experimentName"
 							placeholder="Nome do experimento"
 							type="text"
 							className="input-arredondado"
-							value={nameExperiment}
-							onChange={(e) => setNameExperiment(e.target.value)}
+							value={experimentName}
+							onChange={(e) => setExperimentName(e.target.value)}
 						/>
 					</div>
-					<h3>Adicionar fator</h3>
 					<div>
+						<h3>Adicionar fator</h3>
 						<input
 							type="text"
 							className="input-arredondado"
 							id="factorName"
 							placeholder="Nome do fator"
 						/>
+						<button
+							id="addFactor"
+							className="botao-responsivo"
+							type="button"
+							onClick={() => addFactor(document.getElementById("factorName").value)}
+						>
+							Adicionar
+						</button>
+						<p>Valor para fator</p>
 						<input
 							type="text"
 							className="input-arredondado"
 							id="factorValue"
 							placeholder="Valor do fator"
+							value={value}
+							onChange={(e) => setValue(e.target.value)}
 						/>
-						<button
-							type="button"
-							className="botao-responsivo"
-							onClick={() =>
-								addFactor(
-									document.getElementById("factorName").value,
-									document.getElementById("factorValue").value
-								)
-							}
-						>
-							Adicionar
-						</button>
 					</div>
-					<h3>Adicionar teste</h3>
-					<div>
-						<input
-							type="text"
-							className="input-arredondado"
-							id="trialName"
-							placeholder="Nome do teste"
-						/>
-						<button
-							className="botao-responsivo"
-							type="button"
-							onClick={() => addTrial(document.getElementById("trialName").value)}
-						>
-							Adicionar
-						</button>
-					</div>
-					<button className="botao-responsivo" type="button" onClick={submitExperiment}>
+					<button
+						className="botao-responsivo"
+						type="button"
+						id="createExperiment"
+						onClick={submitExperiment}
+					>
 						Criar experimento
 					</button>
 					<button className="botao-responsivo-secundario" type="button" onClick={clearExperiment}>
@@ -159,62 +138,36 @@ const CreateExperiment = () => {
 					</button>
 				</form>
 			</div>
-			<div className="list-container">
-				<h2>Fatores</h2>
-				<ul>
-					{Object.keys(factors).map((factor) => (
-						<li key={factor}>
-							{factor}: {factors[factor]}{" "}
-							<button
-								type="button"
-								className="botao-responsivo-secundario"
-								onClick={() => removeFactor(factor)}
-							>
-								Remover
-							</button>
-						</li>
-					))}
-				</ul>
-			</div>
 			<div className="trial-container">
-				<h2>Testes</h2>
-				{Object.keys(trials).map((trial) => (
-					<div key={trial} className="trail">
+				<h2>Fatores</h2>
+				{Object.keys(factors).map((factor) => (
+					<div key={factor} className="trail">
 						<div className="trail-row">
-							{trial}
+							{factor}
 							<div>
-								<div className="dropdown">
-									<div role="button" className="botao-responsivo">
-										Adicionar fator
-									</div>
-									<div className="dropdown-content">
-										{Object.keys(factors).map((factor) => (
-											<span
-												key={factor}
-												role="button"
-												onClick={() => addFactorToTrial(trial, factor)}
-											>
-												{factor}
-											</span>
-										))}
-									</div>
+								<div
+									onClick={() => addValueToFactor(factor, value)}
+									role="button"
+									className="botao-responsivo"
+								>
+									Adicionar valor ao fator
 								</div>
 								<button
 									className="botao-responsivo-secundario"
 									type="button"
-									onClick={() => removeTrial(trial)}
+									onClick={() => removeFactor(factor)}
 								>
 									Remover
 								</button>
 							</div>
 						</div>
-						{trials[trial].map((factor) => (
-							<div key={factor} className="factor-trail">
-								{factor}
+						{factors[factor].map((value) => (
+							<div key={value} className="factor-trail">
+								{value}
 								<button
 									className="botao-responsivo-secundario"
 									type="button"
-									onClick={() => removeFactorFromTrial(trial, factor)}
+									onClick={() => removeValueFromFactor(factor, value)}
 								>
 									Remover
 								</button>
